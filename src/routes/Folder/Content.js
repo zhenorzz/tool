5ed85600b552cec7e1a951ed401050cf2fa8dce4
johnsonzz <liz@uclbrt.com>
2017-12-13 09:35:56 +0800

    button

--- src/routes/Folder/Content.js
+++ src/routes/Folder/Content.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
-
+import {Upload, Icon, Divider, Button } from 'antd';
+const ButtonGroup = Button.Group;
 class FolderContent extends Component {
     render() {
         return (
-            <div>nihao</div>
+            <div>
+                <section id="button-section">
+                <Button style={{ marginRight: 8 }}>根目录</Button>
+                <Upload name="logo" listType="picture" className="inline-block" style={{ marginRight: 8 }}>
+                    <Button>
+                        <Icon type="upload" /> 选择要上传的文件
+                    </Button>
+                </Upload>
+                <Button icon="folder" style={{ marginRight: 8 }}>
+                   新建文件夹
+                </Button>
+                <ButtonGroup style={{ marginRight: 8 }}>
+                    <Button icon="download" />
+                    <Button icon="qrcode" />
+                    <Button icon="eye" />
+                    <Button icon="delete" />
+                </ButtonGroup>
+                </section>
+                <Divider />
+            </div>
         );
     }
 }
