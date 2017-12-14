d50ed0e9b2bade60f6cf8686bcabee739fa30385
johnsonzz <liz@uclbrt.com>
2017-12-14 17:42:18 +0800

    目录

--- src/routes/Folder/Content.js
+++ src/routes/Folder/Content.js
@@ -7,7 +7,7 @@ class FolderContent extends Component {
         super(pros);
     }
 
-    state = {file: [], dir: []};
+    state = {file: [], dir: [], folder: ['根目录']};
 
     componentDidMount() {
         axios.get('http://localhost:6324/index/Index/read', {params: {path: ''}})
@@ -22,21 +22,48 @@ class FolderContent extends Component {
             })
     }
 
-    handleClick = (event) => {
-        axios.get('http://localhost:6324/index/Index/read', {params: {path: ''}})
+    handleClick(item, index, event) {
+        let folders = this.state.folder;
+        folders = folders.slice(0, index + 1);
+        this.setState({
+            folder: folders,
+        });
+        let path = '';
+        for (let i = 1; i < folders.length; i++) {
+            path += folders[i] + '/'
+        }
+        axios.get('http://localhost:6324/index/Index/read', {params: {path: path}})
             .then((response) => {
                 let data = JSON.parse(response.request.response);
                 let file = data.file;
                 let dir = data.dir;
                 this.setState({
-                    file: file,
-                    dir: dir,
+                    file: file ? file : [],
+                    dir: dir ? dir : [],
                 });
             })
     }
 
-    dirClick(index) {
-        console.log(index);
+    dirClick(item) {
+        let folders = this.state.folder;
+        folders.push(item);
+        this.setState({
+            folder: folders,
+        });
+        let path = '';
+        for (let i = 1; i < folders.length; i++) {
+            path += folders[i] + '/'
+        }
+        axios.get('http://localhost:6324/index/Index/read', {params: {path: path}})
+            .then((response) => {
+                let data = JSON.parse(response.request.response);
+                let file = data.file;
+                let dir = data.dir;
+                this.setState({
+                    file: file ? file : [],
+                    dir: dir ? dir : [],
+                });
+            })
     }
 
     render() {
@@ -44,7 +71,17 @@ class FolderContent extends Component {
         return (
             <div>
                 <section id="button-section">
-                    <Button style={{marginRight: 8}} onClick={this.handleClick}>根目录</Button>
+                    <ButtonGroup style={{marginRight: 8}}>
+                        {
+                            this.state.folder.map((item, index) => {
+                                return (
+                                    <Button key={index}
+                                            onClick={this.handleClick.bind(this, item, index)}>{item}</Button>
+                                )
+                            })
+                        }
+                    </ButtonGroup>
+
                     <Upload name="logo" listType="picture" className="inline-block" style={{marginRight: 8}}>
                         <Button>
                             <Icon type="upload"/> 选择要上传的文件
@@ -93,7 +130,8 @@ class FolderContent extends Component {
                             return (
                                 <Col xs={12} sm={6} md={4} lg={2} key={index}>
                                     <div style={{textAlign: 'center'}}>
-                                        <img alt={item} width="64" src={require('../../assets/images/'+suffix+'.png')}/>
+                                        <img alt={item} width="64"
+                                             src={require('../../assets/images/' + suffix + '.png')}/>
                                         <div>{item}</div>
                                     </div>
                                 </Col>
